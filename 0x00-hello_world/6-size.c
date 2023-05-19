bba3a1c08f898410c5c6feac2d8575162114b629
theLawrenzo <solomonlaurens96@gmail.com>
2023-05-18 21:55:00 -0700

    A program that prints out the size of various data-types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int mani(void)
+{
+	int a;
+	char b;
+	float c;
+	double d;
+
+	printf("The size of int = %d\n", sizeof(a));
+	printf("The size of char = %d\n", sizeof(b));
+	printf("The size of float = %d\n", sizeof(c));
+	printf("The size of double = %d\n", sizeof(d));
+
+	return (0);
+}
