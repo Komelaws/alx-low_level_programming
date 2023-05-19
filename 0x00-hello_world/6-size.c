86935eeab4e559d8bd4c75f8e2555289d20517dc
theLawrenzo <solomonlaurens96@gmail.com>
2023-05-18 21:57:54 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,10 @@
  */
 int mani(void)
 {
-	int a;
-	char b;
-	float c;
-	double d;
-
-	printf("The size of int = %d\n", sizeof(a));
-	printf("The size of char = %d\n", sizeof(b));
-	printf("The size of float = %d\n", sizeof(c));
-	printf("The size of double = %d\n", sizeof(d));
+	printf("The size of int = %d\n", sizeof(int));
+	printf("The size of char = %d\n", sizeof(char));
+	printf("The size of float = %d\n", sizeof(float));
+	printf("The size of double = %d\n", sizeof(double));
 
 	return (0);
 }
