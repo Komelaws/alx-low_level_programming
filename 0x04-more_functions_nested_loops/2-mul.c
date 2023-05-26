d0f2b53509833c5d9b8048c0a1f49d30ef316a8f
theLawrenzo <solomonlaurens96@gmail.com>
2023-05-25 20:27:07 -0700

    A program that multiplies two operands

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Multiply two integers
+ * @a: First operand for the multiplication
+ * @b: Second operand for the multiplication
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
