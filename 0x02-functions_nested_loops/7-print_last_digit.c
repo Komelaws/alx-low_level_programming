4af21a5cd22504fd9fafb797097a94daac0a1b56
theLawrenzo <solomonlaurens96@gmail.com>
2023-06-04 18:16:57 -0700

    Update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,19 @@
  *
  * Return: a % 10
  */
-int print_last_digit(int a)
+int print_last_digit(int n)
 {
-	putchar(a % 10);
+	int a;
 
-	return (a % 10);
+	if (n < 0)
+	n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
