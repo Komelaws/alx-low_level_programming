2364a0c73268772caa4fd5b87fa5d37fd28ad3e2
theLawrenzo <solomonlaurens96@gmail.com>
2023-06-23 22:00:00 -0700

    Updat

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,11 @@
 
 /**
  * print_name - function that prnts a name
+ * @name: Input namem
  *
  * Return: Always 0
  */
-void print_name(char *name,__attribute__((unused))void (*f)(char *))
+void print_name(char *name, __attribute__((unused))void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
