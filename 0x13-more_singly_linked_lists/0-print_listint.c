fd3595cf72037645e4282892ca8ed30a2ab0b2bd
Komelaws <solomonlaurens96@gmail.com>
2023-07-05 21:35:45 -0700

    Singly linked list project 0 - function that prints all its elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_listint - A function that prints the number of elements
+ * in a list
+ * @h: The first element of the list
+ *
+ * Return: The number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	listint_t *head;
+	listint_t *new;
+	h = "";
+}
