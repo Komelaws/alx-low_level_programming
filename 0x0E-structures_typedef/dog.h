227be7825063ebfe7754a74617baa04527f0d602
theLawrenzo <solomonlaurens96@gmail.com>
2023-09-11 18:11:18 -0700

    Function that create a new type struct do

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog profile
+ * @name: Name of the dog
+ * @age: The age of the dog
+ * @owner: Name of the owner
+ *
+ * Desription: The full details of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
