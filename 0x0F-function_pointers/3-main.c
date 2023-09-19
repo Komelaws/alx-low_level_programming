df6287e4d23ebb432fea8c20789d285e9b3601e7
theLawrenzo <solomonlaurens96@gmail.com>
2023-09-18 19:19:50 -0700

    Update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
 	int calc;
 	int num1;
 	int num2;
+	char *operator;
 	int (*fptr)(int a, int b);
 
 	if (argc != 4)
@@ -22,12 +23,19 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+	operator = argv[2];
 
-	fptr = get_op_func(argv[2]);
+	fptr = get_op_func(operator);
 	if (!(fptr))
 	{
 		printf("Error\n");
 		exit(99);
+	}
+
+	if (((*operator == '/') || (*operator == '%')) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
 	}
 
 	calc = fptr(num1, num2);
