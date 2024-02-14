e991e103ab06994b11c83039bf48e2989c0a8164
Komelawz <solomonlaurens96@gmail.com>
2024-02-13 17:25:49 -0800

    Function that reads from an text file and print to STDOUT

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,22 +1,31 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * read_textfile - Function that reads a text file and prints it the
  * POSIX standard output
- * @filename: The name of the file to rad text from
- * @letters: The size of the text to read
+ * @filename: Filename
+ * @letters: Number of letters to read
  *
  * Return: num
  */
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t num;
-	FILE *fp;
+	int fd;
+	ssize_t num_read, num_write;
+	char *str;
 
 	if (filename == NULL)
 		return (0);
-
-	fp = open(filename, 'r');
-	if (fp == NULL)
+	if ((fd = open(filename, O_RDWR)) == -1)
 		return (0);
+	str = malloc(letters + 1);
+	if (str == NULL)
+		return (0);
+	if ((num_read = read(fd, str, letters)) == -1)
+		return (0);
+	if ((num_write = write(STDOUT_FILENO, str, (size_t)num_read)) == -1)
+		return (0);
+	free(str);
+	close(fd);
+	return (num_write);
 }
